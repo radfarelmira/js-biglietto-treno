cb8342ffea38aee5de7669727c5319d4a9d6bbac
radfarelmira <radfar.elmira@email.com>
2021-11-12 16:46:38 +0100

    add new var name totalPrice

--- js/script.js
+++ js/script.js
@@ -8,35 +8,30 @@
 //DECLARATION
 //chiedere all'utente il numero di chilometri che vuole percorrere
 const numberOfkm = parseInt ( prompt ('Quanti km è lontana la tua destinazione?'));
-console.log (numberOfkm);
+
 //chiedere all'utente l'età del passeggero
 const userAge = parseInt ( prompt ('Qual è la tua età?'));
-console.log (userAge);
 
 //LOGIC
 // Calcolare il prezzo del biglietto del viaggio
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 let price = (numberOfkm * 0.21);
-console.log (price);
 
 //Se il passeggero è i minorenni va applicato uno sconto del 20% 
 // Se il passeggero ha over 65 anni va applicato uno sconto del 40%
-let totalPrice;
 
 if ( userAge <= 18){
-    totalPrice = (price * 20) / 100;
+    price = (price * 20) / 100;
 
 } else if ( userAge >= 65){
-    totalPrice = (price * 40) / 100;
+    price = (price * 40) / 100;
 
 }
-console.log (totalPrice);
 
 // OUTPUT
 // L'output del prezzo finale va messo fuori in forma umana
-totalPrice = price.toFixed(2).toString();
-finalPrice = `${totalPrice} €`;
-console.log (price);
+let totalPrice = price.toFixed(2).toString(); 
+totalPrice = `${totalPrice} €`;
 
-alert (finalPrice);
-console.log (finalPrice);
+// Output finale
+alert (totalPrice);
